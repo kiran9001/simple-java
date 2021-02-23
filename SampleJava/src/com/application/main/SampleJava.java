74be155039982ee7a5c07105101259c8813f1e20
libra <libra@Amazonforest>
2021-02-24 01:06:01 +0530

    for loop

--- SampleJava/src/com/application/main/SampleJava.java
+++ SampleJava/src/com/application/main/SampleJava.java
@@ -4,7 +4,19 @@ public class SampleJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world !");
+		//System.out.println("Hello world !");
+		
+		//for loop
+		for(int i=0; i<5; i++) {
+			System.out.println("current value: "+i);
+		}
+		System.out.println("######################");
+		
+		for(int i=0; i<=5; i++) {
+			System.out.println("current value: "+i);
+		}
+		
+		
 	}
 
 }
