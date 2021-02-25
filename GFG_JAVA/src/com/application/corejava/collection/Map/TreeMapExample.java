bd3d6ecf583da57e587205cf340f3ea2a4f3011c
libra <libra@Amazonforest>
2021-02-25 14:07:45 +0530

    no changes

--- GFG_JAVA/src/com/application/corejava/collection/Map/TreeMapExample.java
+++ GFG_JAVA/src/com/application/corejava/collection/Map/TreeMapExample.java
@@ -18,10 +18,12 @@ public class TreeMapExample {
 		m.put("one", 1);
 		m.put("two", 2);
 		m.put("seven", 7);
+		
 		System.out.print("Value Associated with key 'two'= "+m.get("two")+"\n");
 		System.out.print("Keysets: "+m.keySet()+"\n");
 		System.out.print("Entry of map: "+m.entrySet()+"\n");
 		System.out.println("Size of map: "+m.size());
+		
 		if(m.containsKey("one")) {
 			Integer x=m.get("one");
 			System.out.println("one : "+x);
